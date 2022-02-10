5cdff564ebbf918e008b6107ea94fbdcdac18dc5
Rajaboff <rajabov.manuchehr2001@gmail.com>
2022-02-10 20:54:11 +0500

    add auth validations

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { of, from } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  credentials = {
+    email: '',
+    password: ''
+  }
+
+  observable = of(1,2,3,4,5)
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    // console.log(this.credentials);
+
+    const subscription = this.observable.subscribe({
+      next(value) {
+        console.log(value)
+      },
+      complete() {
+        console.log('complete')
+      }
+    })
   }
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,ts}"],
+    theme: {
+        extend: {
+            opacity: ['disabled'],
+            backgroundColor: ['disabled']
+        },
+    },
+    plugins: [],
 }
